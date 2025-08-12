13c930fbd5be4dbca8faa7f271ddd7c32d77350d
Leonardo <leonardo10sp@gmail.com>
2025-08-12 15:26:29 -0300

    feat: create CI/CD

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,51 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
-  testMatch: [
-    '**/__tests__/**/*.ts',
-    '**/?(*.)+(spec|test).ts'
-  ],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
   transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      tsconfig: {
-        outDir: null,
-        esModuleInterop: true
-      }
-    }],
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          outDir: null,
+          esModuleInterop: true,
+        },
+      },
+    ],
   },
   collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts',
-    '!src/index.ts'
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/index.ts",
+    "!src/**/*.interface.ts",
+    "!src/**/*.type.ts",
   ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
-  testTimeout: 10000,
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  verbose: true
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html", "json"],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
+  testTimeout: 15000,
+  moduleFileExtensions: ["ts", "js", "json"],
+  verbose: true,
+  // CI/CD Settings
+  ci: process.env.CI === "true",
+  forceExit: true,
+  detectOpenHandles: true,
+  reporters: process.env.CI
+    ? [
+        ["default"],
+        [
+          "jest-junit",
+          { outputDirectory: "test-results", outputName: "junit.xml" },
+        ],
+      ]
+    : ["default"],
 };
